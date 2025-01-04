001dc483571a3012b31f75b368f6efcc0129f076
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 15:20:52 -0300

    Começando a resolver um segundo problema exemplo

---
+++ Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_CSharp_P2
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
 ﻿
+using POO_CSharp_P2;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
+//        Exercício Exemplo
+//        Fazer um programa para ler os dados de um produto em estoque(nome, preço e
+//        quantidade no estoque). Em seguida:
+//        • Mostrar os dados do produto(nome, preço, quantidade no estoque, valor total no
+//          estoque)
+//        • Realizar uma entrada no estoque e mostrar novamente os dados do produto
+//        • Realizar uma saída no estoque e mostrar novamente os dados do produto
+
+        Produto p = new Produto();
+
+        Console.WriteLine("Entre os dados do produto:");
+        Console.Write("Nome: ");
+        p.Nome = Console.ReadLine();
+        Console.Write("Preço: ");
+        p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        Console.WriteLine("Dados do produto: " + p);
+
     }
 }
